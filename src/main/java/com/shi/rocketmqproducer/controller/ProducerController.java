ab81b13d980c5a0ff3de8d523fbdecd058e0b44b
unknown <1835031977@qq.com>
2019-05-26 18:45:18 +0800

    syk

--- src/main/java/com/shi/rocketmqproducer/controller/ProducerController.java
+++ src/main/java/com/shi/rocketmqproducer/controller/ProducerController.java
@@ -1,5 +1,7 @@
 package com.shi.rocketmqproducer.controller;
 
+import com.google.gson.Gson;
+import com.shi.rocketmqproducer.entity.MessageTemplate;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
@@ -12,8 +14,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.json.Json;
 
 import java.io.UnsupportedEncodingException;
+import java.util.UUID;
 
 /**
  * @ClassName ProducerController
@@ -30,17 +34,46 @@ public class ProducerController {
     @Autowired
     private DefaultMQProducer defaultMQProducer;
 
-    @GetMapping("send")
-    public void send() throws MQClientException, RemotingException, MQBrokerException, InterruptedException, UnsupportedEncodingException {
-        // 创建消息，并指定Topic，Tag和消息体
-        Message msg = new Message("Topic-2" /* Topic */,
-                "Tag-2","Key-2",
-                ("Hello RocketMQ ShiYaKun11").getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
-        );
-        // 发送消息到一个Broker
-        SendResult sendResult = defaultMQProducer.send(msg);
+    @GetMapping("send_tag1")
+    public void sendTag1() throws MQClientException, RemotingException, MQBrokerException, InterruptedException, UnsupportedEncodingException {
+        // 创建消息，并指定Topic，Tag和消息体\
+
+            MessageTemplate messageTemplate = new MessageTemplate();
+            messageTemplate.setId(UUID.randomUUID().toString());
+            messageTemplate.setSendMessageType("Tag-1");
+            messageTemplate.setMessageinfo("测试消息队列压力");
+            Gson gson = new Gson();
+            String s = gson.toJson(messageTemplate);
+            Message msg = new Message("Topic-2" /* Topic */,
+                    "Tag-1","Key-1",
+                    s.getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            // 发送消息到一个Broker
+            SendResult sendResult = defaultMQProducer.send(msg);
+
+
         // 通过sendResult返回消息是否成功送达
         System.out.printf("%s%n", sendResult);
+        // 如果不再发送消息，关闭Producer实例。
+    }
+
+    @GetMapping("send_tag2")
+    public void sendTag2() throws MQClientException, RemotingException, MQBrokerException, InterruptedException, UnsupportedEncodingException {
+        // 创建消息，并指定Topic，Tag和消息体
+        for (int i = 0; i <10; i++) {
+            MessageTemplate messageTemplate = new MessageTemplate();
+            messageTemplate.setId(UUID.randomUUID().toString());
+            messageTemplate.setSendMessageType("Tag-2");
+            messageTemplate.setMessageinfo("测试消息队列压力"+i);
+            Gson gson = new Gson();
+            String s = gson.toJson(messageTemplate);
+            Message msg = new Message("Topic-3" /* Topic */,
+                    "Tag-2","Key-2",
+                    s.getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            // 发送消息到一个Broker
+            SendResult sendResult = defaultMQProducer.send(msg);
+        }
 
         // 如果不再发送消息，关闭Producer实例。
 
